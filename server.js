f005eba6fc10723adb2521f6a42938ef9e352a0c
deldridge <74927735+dillon-eldridge@users.noreply.github.com>
2023-03-01 16:34:21 -0700

    Connecting to mongo utilizing secrets

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import Nullstack from 'nullstack'
 
 import Application from './src/Application'
-const { MongoClient } = require('mongodb');
+
+const { MongoClient } = require('mongodb')
 
 const context = Nullstack.start(Application)
 
 context.start = async function start() {
+  const { secrets } = context
   try {
-    const databaseClient = new MongoClient('mongodb://localhost:27017')
-    await databaseClient.connect();
-    context.database = await databaseClient.db('nft-marketplace')
+    console.log('[INFO] Connecting to database...')
+    const databaseClient = new MongoClient(secrets.mongodbUri)
+    await databaseClient.connect()
+    context.database = await databaseClient.db(secrets.databaseName)
+    console.log(`[INFO] Connected to database ${secrets.databaseName}`)
   } catch (error) {
     console.error(error)
   }
