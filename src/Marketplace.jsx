2c2a83cef3818a87497a9a2b792ae9d3cb94eb2e
deldridge <74927735+dillon-eldridge@users.noreply.github.com>
2023-03-02 14:53:32 -0700

    Implemented TW on Marketplace Component

--- src/Marketplace.jsx
+++ src/Marketplace.jsx
@@ -3,19 +3,24 @@ import Nullstack from 'nullstack'
 import './Marketplace.scss'
 
 class Marketplace extends Nullstack {
+  prepare({ page }) {
+    page.title = 'Marketplace'
+    page.description = 'Find the latest and greatest NFTs from top artists.'
+  }
+
   nfts = []
 
   static async getNFTs({ database }) {
     return await database.collection('nfts').find().toArray()
   }
 
-  async initiate() {
+  async initiate({ params }) {
     this.nfts = await this.getNFTs()
   }
 
   render() {
     return (
-      <div class="grid">
+      <div class="grid grid-cols-3 gap-x-6">
         {this.nfts.map((nft) => (
           <div class="nft__tile">
             {/* <div
@@ -24,8 +29,12 @@ class Marketplace extends Nullstack {
               }}
             ></div> */}
             <img src={nft.properties.image.description} />
-            <h4>{nft.title}</h4>
-            <button>Buy</button>
+            <div class="flex align-start flex-col space-y-4 p-6">
+              <p class="text-xl font-black">{nft.title}</p>
+              <button class="bg-green-900 text-white rounded-full py-2 px-4 w-full">
+                Buy @ <span>{nft.price} ETH</span>
+              </button>
+            </div>
           </div>
         ))}
       </div>
